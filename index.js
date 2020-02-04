55a1f098f1e121d53d135379bcf357d26592a2f2
manel turki <menelturki@gmail.com>
2020-02-04 09:30:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,12 +6,21 @@ const record = [
 function isWinner (obj){
   return obj.result==="W";
 }
+<<<<<<< HEAD
 function superbowlWin (record){
   if(record.find(isWinner)){
     let s=record.find(isWinner);
+=======
+function superbowlWIN (record){
+  if(record.find(isWinner)){
+>>>>>>> f2db2c281424e7b35a10837bc43ca1c81ae457cd
 return s.year;
 }
 else return undefined;
 
+<<<<<<< HEAD
 superbowlWin(record)
+=======
+superbowlwin(record)
+>>>>>>> f2db2c281424e7b35a10837bc43ca1c81ae457cd
 }
